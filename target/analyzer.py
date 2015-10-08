91a7c41b3ac0e0efe995252ad933f3754b63f0cf
John Phillips <john.h.phillips@clegadgetworks.com>
2015-10-07 20:35:53 -0700

    Added license statement

--- target/analyzer.py
+++ target/analyzer.py
@@ -1,3 +1,19 @@
+# target-analyzer, functions to support analysis of contact information 
+# Copyright (C) 2015 John Phillips, SPAWAR Systems Center Pacific
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from math import radians, sin, cos, asin, sqrt, pow, atan2, degrees
 from myattributes import *
 import xml.etree.ElementTree as ET
--- target/gui_sandbox.py
+++ target/gui_sandbox.py
@@ -1,44 +1,74 @@
+# target-analyzer gui_sandbox 
+# Copyright (C) 2015 John Phillips, SPAWAR Systems Center Pacific
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from Tkinter import *
 
-#------------------------------------
+# #------------------------------------
+# 
+# def addBox():
+#     print "ADD"
+# 
+#     frame = Frame(root)
+#     frame.pack()
+# 
+#     Label(frame, text='From').grid(row=0, column=0)
+# 
+#     ent1 = Entry(frame)
+#     ent1.grid(row=1, column=0)
+# 
+#     Label(frame, text='To').grid(row=0, column=1)
+# 
+#     ent2 = Entry(frame)
+#     ent2.grid(row=1, column=1)
+# 
+#     all_entries.append( (ent1, ent2) )
+# 
+# #------------------------------------
+# 
+# def showEntries():
+# 
+#     for number, (ent1, ent2) in enumerate(all_entries):
+#         print number, ent1.get(), ent2.get()
+# 
+# #------------------------------------
+# 
+# all_entries = []
+# 
+# root = Tk()
+# 
+# showButton = Button(root, text='Show all text', command=showEntries)
+# showButton.pack()
+# 
+# addboxButton = Button(root, text='<Add Time Input>', fg="Red", command=addBox)
+# addboxButton.pack()
+# 
+# root.mainloop()
 
-def addBox():
-    print "ADD"
+from Tkinter import *
 
-    frame = Frame(root)
-    frame.pack()
+main = Tk()
 
-    Label(frame, text='From').grid(row=0, column=0)
+def leftKey(event):
+    print "Left key pressed"
 
-    ent1 = Entry(frame)
-    ent1.grid(row=1, column=0)
+def rightKey(event):
+    print "Right key pressed"
 
-    Label(frame, text='To').grid(row=0, column=1)
-
-    ent2 = Entry(frame)
-    ent2.grid(row=1, column=1)
-
-    all_entries.append( (ent1, ent2) )
-
-#------------------------------------
-
-def showEntries():
-
-    for number, (ent1, ent2) in enumerate(all_entries):
-        print number, ent1.get(), ent2.get()
-
-#------------------------------------
-
-all_entries = []
-
-root = Tk()
-
-showButton = Button(root, text='Show all text', command=showEntries)
-showButton.pack()
-
-addboxButton = Button(root, text='<Add Time Input>', fg="Red", command=addBox)
-addboxButton.pack()
-
-root.mainloop()
-
-#------------------------------------
+frame = Frame(main, width=100, height=100)
+main.bind('<Left>', leftKey)
+main.bind('<Right>', rightKey)
+frame.pack()
+main.mainloop()
